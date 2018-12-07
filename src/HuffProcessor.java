c64352760427c0ce497a4cbb441d3ed84ef316d4
Ana Martinez <ana.martinez3@duke.edu>
2018-12-07 01:36:35 -0500

    fix null pointer in compress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -72,7 +72,7 @@ public class HuffProcessor {
 	private void writeHeader(HuffNode root, BitOutputStream out) {
 		// TODO Auto-generated method stub
 		if(root==null) return;
-		if (root.myLeft==null || root.myRight==null) {
+		if (root.myLeft!=null || root.myRight!=null) {
 			out.writeBits(1, 0);
 			writeHeader(root.myLeft, out);
 			writeHeader(root.myRight, out);
